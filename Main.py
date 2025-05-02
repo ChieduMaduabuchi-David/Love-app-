56aad0ed912b00b13c7cdb836fb85498dc10e39a
BlackwhiteFire <maduabuchidavid25@gmail.com>
2025-05-02 12:10:36 +0200

    second test commit

--- Main.py
+++ Main.py
@@ -2,10 +2,16 @@ class Person:
     def __init__(self, id: int, name: str, birthdate: int,image: str, sex: str,location: str ):
         self.id = id
         self.name = name
-        self.birthdate =birthdate
+        self.birthdate = birthdate
         self.image = image
         self.sex = sex
         self.location = location
+        self.nationality = None
+        self.height = None
+        self.Religion = None
+
+    def age(self):
+
 
 
 
